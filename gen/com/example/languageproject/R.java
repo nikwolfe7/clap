954c7f5b4df5532a4d5d8edf4d26502fe6e79fdc
Alex Eveleth <eveleth.alex@gmail.com>
2013-07-27 15:20:30 +0000

    Layout changes

--- gen/com/example/languageproject/R.java
+++ gen/com/example/languageproject/R.java
@@ -32,21 +32,20 @@ public final class R {
         public static final int previous=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000e;
-        public static final int btnNext=0x7f090004;
-        public static final int btnPlay=0x7f090003;
-        public static final int btnPrevious=0x7f090002;
-        public static final int button1=0x7f09000b;
-        public static final int button2=0x7f09000c;
-        public static final int button3=0x7f09000d;
-        public static final int country_item_id=0x7f090005;
-        public static final int country_list=0x7f090006;
-        public static final int language_item_id=0x7f090007;
-        public static final int language_list=0x7f090008;
-        public static final int lesson_item_id=0x7f090009;
-        public static final int lesson_list=0x7f09000a;
-        public static final int main_icon=0x7f090000;
-        public static final int screen=0x7f090001;
+        public static final int btnNext=0x7f080004;
+        public static final int btnPlay=0x7f080003;
+        public static final int btnPrevious=0x7f080002;
+        public static final int button1=0x7f08000b;
+        public static final int button2=0x7f08000c;
+        public static final int button3=0x7f08000d;
+        public static final int country_item_id=0x7f080005;
+        public static final int country_list=0x7f080006;
+        public static final int language_item_id=0x7f080007;
+        public static final int language_list=0x7f080008;
+        public static final int lesson_item_id=0x7f080009;
+        public static final int lesson_list=0x7f08000a;
+        public static final int main_icon=0x7f080000;
+        public static final int screen=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -59,11 +58,6 @@ public final class R {
         public static final int lesson_item=0x7f030007;
         public static final int lesson_list=0x7f030008;
         public static final int main_menu=0x7f030009;
-    }
-    public static final class menu {
-        public static final int lesson_list=0x7f080000;
-        public static final int main=0x7f080001;
-        public static final int play_audio=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
