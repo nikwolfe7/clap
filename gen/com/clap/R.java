b37298a97cdd37758b50b8f03aa74570074ccb2c
Alex Eveleth <eveleth.alex@gmail.com>
2013-08-01 12:11:06 +0000

    fixed the item pressed background color

--- gen/com/clap/R.java
+++ gen/com/clap/R.java
@@ -31,12 +31,15 @@ public final class R {
         public static final int button_default=0x7f020004;
         public static final int button_pressed=0x7f020005;
         public static final int ic_launcher=0x7f020006;
-        public static final int next=0x7f020007;
-        public static final int pause=0x7f020008;
-        public static final int pc_logo=0x7f020009;
-        public static final int play=0x7f02000a;
-        public static final int previous=0x7f02000b;
-        public static final int trannyplay=0x7f02000c;
+        public static final int item_bg=0x7f020007;
+        public static final int item_default=0x7f020008;
+        public static final int item_pressed=0x7f020009;
+        public static final int next=0x7f02000a;
+        public static final int pause=0x7f02000b;
+        public static final int pc_logo=0x7f02000c;
+        public static final int play=0x7f02000d;
+        public static final int previous=0x7f02000e;
+        public static final int trannyplay=0x7f02000f;
     }
     public static final class id {
         public static final int back=0x7f09000e;
